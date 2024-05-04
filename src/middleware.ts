bccaf0b0fc1c8f2394462fef6995210a4b2fbbcb
Rohin Chandra <rohinchandra@gmail.com>
2024-05-04 16:22:44 -0400

    Basic embedding is functional

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,6 @@ export function middleware(request: NextRequest) {
   // If you have a custom domain add it below to the
   // space separated frame-ancestors list.
   const cspHeader = `
-    default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
@@ -17,10 +16,24 @@ export function middleware(request: NextRequest) {
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/;
+    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/ https://*.airtable.com/ ;
     block-all-mixed-content;
     upgrade-insecure-requests;
 `;
+
+// const cspHeader = `
+// default-src 'self';
+// script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+// style-src 'self' 'nonce-${nonce}';
+// img-src 'self' blob: data:;
+// font-src 'self';
+// object-src 'none';
+// base-uri 'self';
+// form-action 'self';
+// frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/ https://airtable.com/ ;
+// block-all-mixed-content;
+// upgrade-insecure-requests;
+// `;
   // Replace newline characters and spaces
   const contentSecurityPolicyHeaderValue = cspHeader
     .replace(/\s{2,}/g, ' ')
