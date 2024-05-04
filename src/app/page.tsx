455c79e497119f47639494fe702267ddaa87037b
Rohin Chandra <rohinchandra@gmail.com>
2024-05-04 14:50:01 -0400

    First Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,9 @@ async function Content({ searchParams }: { searchParams: SearchParams }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h2>
+          Rohin is here
+        </h2>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome to the custom app&nbsp;
           <code className="font-mono font-bold">
